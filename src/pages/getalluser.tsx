8a1e0e3cd63ecf1c12149a30e322f25bb621a7ed
phea <rith.sophea@institute.pse.ngo>
2025-05-02 14:56:48 +0700

    more

--- src/pages/getalluser.tsx
+++ src/pages/getalluser.tsx
@@ -14,7 +14,6 @@ function Users() {
       <h2>Users</h2>
       {users.map((user) => (
         <div
-          key={user.id}
           style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}
         >
           <p>
